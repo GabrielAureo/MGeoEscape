4eb569e6363b1cfa79de796e5d0d8b34147b86c3
Gabriel Aureo <gabrielaureo@gmail.com>
2019-07-26 17:30:55 -0300

    Deletando scripts antigos e ajustando rotação de objetos segurados

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -11,19 +11,21 @@ public class Movable : ARInteractable
     private Vector3 originalPosition;
     private Rigidbody rb;
     Coroutine followRoutine;
+    TrackableBehaviour trackableBehaviour;
     void Start(){
         originalParent = transform.parent;
         originalRotation = transform.localRotation;
         originalPosition = transform.localPosition;
         rb = GetComponent<Rigidbody>();
+        trackableBehaviour = GetComponentInParent<TrackableBehaviour>();
     }
     public override void onHold(ARTouchController controller)
     {
         transform.parent = null;
         //transform.rotation = Quaternion.identity;
+        transform.rotation = originalRotation;
         rb.isKinematic = false;
-        controller.FollowTouch(rb);
-        
+        controller.FollowTouch(rb);        
     }
 
     public override void onRelease(ARTouchController controller)
@@ -32,7 +34,23 @@ public class Movable : ARInteractable
         transform.localRotation = originalRotation;
         transform.localPosition = originalPosition;
         rb.isKinematic = true;
+        if(trackableBehaviour.CurrentStatus == TrackableBehaviour.Status.NO_POSE){
+            DisableObject();
+        }
         controller.ReleaseFollow();
+    }
+
+    private void DisableObject(){
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
     }
 
     public override void onTap(ARTouchController controller)
